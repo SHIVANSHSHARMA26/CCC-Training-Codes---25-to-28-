dbe3ec1f893855163c2941b6d98f0d08a6c6b615
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 01:31:15 +0530

    Character Type Functions

---
+++ file57.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	char ch;
+	scanf("%c", &ch);
+    if(ch >= 'A' && ch <= 'Z'){
+	printf("%c",tolower(ch));
+        }
+    else if(ch >= 'a' && ch <= 'z') {
+        printf("%c",toupper(ch));}
+
+	return 0;
+}
