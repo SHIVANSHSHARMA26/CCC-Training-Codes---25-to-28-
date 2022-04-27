7bcd9503e237cb827ed1406a8f374495cb3541f7
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 01:01:44 +0530

    Cut the Fruit

---
+++ file27.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%2==0 && a>2)
+        printf("YES");
+    else
+        printf("NO");
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
