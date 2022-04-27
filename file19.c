acf19a92d71c8852067d7a7fc0da95b917dca2b4
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 00:54:17 +0530

    MULTIPLE'S OF 4

---
+++ file19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    if (n%4==0){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
+    return 0;
+}
