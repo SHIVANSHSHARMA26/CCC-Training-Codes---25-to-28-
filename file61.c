b889661a20ac3dbffb8f341693b666e672281c4f
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 01:34:15 +0530

    Natural Numbers Recursively

---
+++ file61.c
@@ -0,0 +1,51 @@
+#include<stdio.h>  
+
+ 
+
+void display(int);  
+
+ 
+
+int main()  
+
+{  
+
+    int limit;  
+
+ 
+
+   
+
+    scanf("%d", &limit);  
+
+ 
+
+   
+
+    display(limit);  
+
+ 
+
+    return 0;  
+
+}  
+
+ 
+
+void display(int num)  
+
+{  
+
+    if(num)  
+
+        display(num-1);  
+
+    else  
+
+        return;  
+
+ 
+
+    printf("%d ", num);  
+
+}  
