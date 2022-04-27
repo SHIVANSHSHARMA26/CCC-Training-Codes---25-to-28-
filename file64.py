c3e4a211d7632e7e5a22c1566149569f36763476
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 01:36:21 +0530

    Add Two Values

---
+++ file64.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+x,y = list(map(float,input().split(' ')))
+
+if int(x+y)==float(x+y):
+
+    print(int(x+y))
+
+else:
+
+    print(x+y)
+
