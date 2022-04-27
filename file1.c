7789d3c700da7b0ff5a74ebd3fe093fc66024657
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 00:32:12 +0530

    The If-Else Statement
    Hacker rank -CCC training (25 to 28)

---
+++ file1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1, var2;
+    scanf("%d %d",&var1,&var2);
+    
+	//input var1 and var2, compare them and print result accordingly
+if (var1<var2)
+{
+    printf("Value1 is less than Value2");
+}
+else 
+{
+   printf("Value1 is not less than Value2");
+}
+	return 0;
+}
