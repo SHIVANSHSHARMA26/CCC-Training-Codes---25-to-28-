f64931e8bdd9d35a463513941ed05217479d59b2
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 01:22:35 +0530

    Concatenate Two Strings

---
+++ file46.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100],str3[100];
+    scanf("%s%s",str1,str2);
+
+    int i = 0, j = 0;
+    while (str1[i] != '\0') {
+        str3[j] = str1[i];
+        i++;
+        j++;
+    }
+    i = 0;
+    while (str2[i] != '\0') {
+        str3[j] = str2[i];
+        i++;
+        j++;
+    }
+    str3[j] = '\0';
+    printf("%s", str3);
+
+    return 0;
+}
