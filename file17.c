25e5672a9df7192eb58c37289bdae54450d7dd23
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2022-04-28 00:52:27 +0530

    Grading System

---
+++ file17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+    scanf("%d", &marks);
+    if(marks >= 76 && marks <=100){
+        printf("A");}
+    else if(marks >= 51 && marks <= 75){
+        printf("B");}
+    else if(marks >= 26 && marks <= 50){
+        printf("C");}
+    else{
+        printf("D");}
+    return 0;
+}
